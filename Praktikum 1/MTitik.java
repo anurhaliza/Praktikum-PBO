a2e959dd4a93f7fa9c64a0b94c9169937c1972d9
anurhaliza <100413755+anurhaliza@users.noreply.github.com>
2023-03-01 07:31:25 +0700

    Update MTitik.java

--- Praktikum 1/MTitik.java
+++ Praktikum 1/MTitik.java
@@ -1,3 +1,9 @@
+/**
+* Nama File: MTitik.java (22 Maret 2023)
+* Nama/NIM: Ajeng Nurhaliza Ar Rachman/24060121140109
+* Deskripsi: Kelas yang berisi program utama yang memanfaatkan kelas Titik
+**/
+
 class MTitik {
 
 	public static void main(String[] args){
@@ -6,11 +12,12 @@ class MTitik {
 		Titik t3;
 		t1 = new Titik();
 		t2 = new Titik();
-		t3 = new Titik(5,6);
+		t3 = new Titik();
 		t1.setAbsis(4.0);
 		t2.setOrdinat(2.0);
+		t3.setOrdinat(1.0)
 		System.out.println("t1.x = "+t1.getAbsis());
 		System.out.println("t2.x = "+t2.getOrdinat());
-		System.out.println("t3.x = "+t3.new Titik());
+		System.out.println("t3.x = "+t3.getOrdinat());
 	}
 }
